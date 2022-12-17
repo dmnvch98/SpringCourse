918814ca5cee295e8c70a453aafe06255fe11d97
dmnvch98 <dmnvch.evg@gmail.com>
2022-12-18 01:06:03 +0400

    add missing files

--- SocialNetwork/src/main/java/org/example/springmvc/Main.java
+++ SocialNetwork/src/main/java/org/example/springmvc/Main.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.exmaple.security.client")
+@EnableFeignClients(basePackages = "org.exmaple.springmvc.client")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
